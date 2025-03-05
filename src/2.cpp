39bade4993866c54eeb6240eefaefead90cd5e66
bennettdq7nc <bennettdq7nc@gmx.com>
2025-03-05 17:54:21 +0000

    Add 2.cpp

---
+++ src/2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int x = 10;
+    double y = 5.234;
+    std::cout << "The value of x is: " << x << std::endl;
+    std::cout << "The value of y is: " << y << std::endl;
+    return 0;
+}
